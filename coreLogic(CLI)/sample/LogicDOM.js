10db0b9bf5370120381a5388e3295bfff61652f9
praggypraggy <praggypraggy@gmail.com>
2022-06-24 20:42:48 +0530

    Added fade Animations to cells

--- coreLogic(CLI)/sample/LogicDOM.js
+++ coreLogic(CLI)/sample/LogicDOM.js
@@ -51,7 +51,10 @@ const isGameOver = () => {
 
     document.getElementById('gOver').innerHTML = '<h2 style="color:red">GAME OVER</h2>';
     let x = document.getElementById("myTable").getElementsByTagName("td");
-    Array.from(x).forEach((item) => { item.style.backgroundColor = "red"; });
+    Array.from(x).forEach((item) => {
+      item.style.backgroundColor = "red";
+      item.style.color = "white";
+    });
 
   }
   for (let i = 0; i < board.length; i++) {
@@ -63,6 +66,12 @@ const isGameOver = () => {
     }
   }
   gameOvercheck = false;
+};
+
+const callback = (callbackStack, move) => {
+  Array.from(callbackStack).forEach((item) => {
+    item(move);
+  });
 };
 
 const newBuf = () => {
@@ -77,7 +86,7 @@ const showBuf = () => {
   console.log(buf);
 };
 
-const newGame = () => {
+const newGame = (callbackStack, move) => {
   score = 0;
   setScore(score);
   gameOvercheck = false;
@@ -87,8 +96,7 @@ const newGame = () => {
   for (let i = 0; i < 4; i++) {
     board[i] = newBuf();
   }
-  randCoord();
-  showBoardDOM();
+  callback(callbackStack, move);
 };
 
 const setScore = (val) => {
@@ -103,19 +111,72 @@ const scoreUpdate = (val) => {
   }
 }
 
-const callback = (callbackStack) => {
-  Array.from(callbackStack).forEach((item) => {
-    item();
-  });
-};
-
 const showBoard = () => {
   for (let i = 0; i < board.length; i++) {
     console.log(board[i]);
   }
 };
 
-const showBoardDOM = () => {
+async function cellAnimation(x, move) {
+  const fade = { 'W': 'fade-in-bottom', 'A': 'fade-in-right', 'S': 'fade-in-top', 'D': 'fade-in-left', 'N': 'fade-in' };
+  if (move === 'W') {
+    for (let i = 0; i <= 3; i++) {
+      let promise = new Promise((resolve, reject) => {
+        setTimeout(() => resolve('fade-in-text'), 50)
+      });
+      let result = await promise;
+      for (let j = 0; j <= 3; j++) {
+        x[i].children[j].children[0].classList.add(fade[move]);
+      }
+    }
+  }
+  if (move === 'S') {
+    for (let i = 3; i >= 0; i--) {
+      let promise = new Promise((resolve, reject) => {
+        setTimeout(() => resolve('fade-in-text'), 50)
+      });
+      let result = await promise;
+      for (let j = 0; j <= 3; j++) {
+        x[i].children[j].children[0].classList.add(fade[move]);
+      }
+    }
+  }
+  if (move === 'A') {
+    for (let i = 0; i <= 3; i++) {
+      let promise = new Promise((resolve, reject) => {
+        setTimeout(() => resolve('fade-in-text'), 50)
+      });
+      let result = await promise;
+      for (let j = 0; j <= 3; j++) {
+        x[j].children[i].children[0].classList.add(fade[move]);
+      }
+    }
+  }
+  if (move === 'D') {
+    for (let i = 3; i >= 0; i--) {
+      let promise = new Promise((resolve, reject) => {
+        setTimeout(() => resolve('fade-in-text'), 50)
+      });
+      let result = await promise;
+      for (let j = 0; j <= 3; j++) {
+        x[j].children[i].children[0].classList.add(fade[move]);
+      }
+    }
+  }
+  if (move === 'N') {
+    for (let i = 3; i >= 0; i--) {
+      let promise = new Promise((resolve, reject) => {
+        setTimeout(() => resolve('fade-in-text'), 50)
+      });
+      let result = await promise;
+      for (let j = 0; j <= 3; j++) {
+        x[j].children[i].children[0].classList.add(fade[move]);
+      }
+    }
+  }
+}
+
+const showBoardDOM = (move) => {
   mainDiv.innerHTML = ``;
   var table = document.createElement('table');
 
@@ -124,6 +185,7 @@ const showBoardDOM = () => {
     for (let j = 0; j < board[i].length; j++) {
       let x = board[i][j];
       let td = document.createElement('td');
+      let span = document.createElement('span');
       if (x > 512) {
         td.style.backgroundColor = 'red';
       } else if (x > 128) {
@@ -138,24 +200,26 @@ const showBoardDOM = () => {
         td.style.backgroundColor = '#b1ff03';
       }
       if (x === 0) x = " ";
-      td.innerHTML = x;
+      span.innerHTML = x;
+      span.className = 'spanBox';
       td.style.border = "1px solid red";
       td.className = 'box';
+      td.appendChild(span);
       tr.appendChild(td);
     }
     table.appendChild(tr);
   }
-  console.log(mainDiv);
   table.id = 'myTable';
-  mainDiv.appendChild(table)
+  mainDiv.appendChild(table);
+  var x = table.children;
+  cellAnimation(x, move);
 };
 
 const closeGame = () => {
   mainDiv.innerHTML = `<h3 style='color:red'>Game closed</h3>`
 };
 
-const moveUp = (callbackStack) => {
-  console.log('Move Up\n');
+const moveUp = (callbackStack, move) => {
   for (let j = 0; j < 4; j++) {
     buf = newBuf();
     let k = 0;
@@ -175,11 +239,10 @@ const moveUp = (callbackStack) => {
       board[i][j] = buf[i];
     }
   }
-  callback(callbackStack);
+  callback(callbackStack, move);
 };
 
-const moveDown = (callbackStack) => {
-  console.log('Move Down\n');
+const moveDown = (callbackStack, move) => {
   for (let j = 0; j < 4; j++) {
     buf = newBuf();
     let k = 3;
@@ -199,11 +262,10 @@ const moveDown = (callbackStack) => {
       board[i][j] = buf[i];
     }
   }
-  callback(callbackStack);
+  callback(callbackStack, move);
 };
 
-const moveLeft = (callbackStack) => {
-  console.log('Move Left\n');
+const moveLeft = (callbackStack, move) => {
   for (let i = 0; i < 4; i++) {
     buf = newBuf();
     let k = 0;
@@ -223,11 +285,10 @@ const moveLeft = (callbackStack) => {
       board[i][j] = buf[j];
     }
   }
-  callback(callbackStack);
+  callback(callbackStack, move);
 };
 
-const moveRight = (callbackStack) => {
-  console.log('Move Right\n');
+const moveRight = (callbackStack, move) => {
   for (let i = 0; i < 4; i++) {
     buf = newBuf();
     let k = 3;
@@ -247,19 +308,20 @@ const moveRight = (callbackStack) => {
       board[i][j] = buf[j];
     }
   }
-  callback(callbackStack);
+  callback(callbackStack, move);
 };
 
 function main(move) {
   const callbackStack = [randCoord, showBoardDOM, isGameOver];
   var choice = String(move);
-  const switchStack = { 'W': moveUp, 'A': moveLeft, 'S': moveDown, 'D': moveRight };
+  const switchStack = { 'W': moveUp, 'A': moveLeft, 'S': moveDown, 'D': moveRight, 'N': newGame, 'L': closeGame };
   if (choice === "L") {
     loop = 0;
     console.log('Closing Game');
+    switchStack[choice](move);
   }
   if (choice in switchStack) {
-    switchStack[choice](callbackStack);
+    switchStack[choice](callbackStack, move);
   }
   else {
     console.log('Enter the correct Move');
@@ -325,31 +387,36 @@ function checkKey(e) {
   e = e || window.event;
   switch (String(e.keyCode)) {
     case '78':
-      newGame();
+      main('N');
+      console.log('New Game\n');
       break;
 
     case '76':
-      closeGame();
+      main('L');
       break;
 
     case '38':
     case '87':
       main('W');
+      console.log('Move Up\n');
       break;
 
     case '40':
     case '83':
       main('S');
+      console.log('Move Down\n');
       break;
 
     case '37':
     case '65':
       main('A');
+      console.log('Move Left\n');
       break;
 
     case '39':
     case '68':
       main('D');
+      console.log('Move Right\n');
       break;
 
     default:
